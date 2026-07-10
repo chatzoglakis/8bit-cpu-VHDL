f514eb8a511f5f5fb2e4d71438bebc2e3dc3eb3d
Michael <mihalishatzoglakis@gmail.com>
2026-07-10 23:55:40 +0300

    Add files via upload

---
+++ simulation/reg_tb.vhd
@@ -0,0 +1,62 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.NUMERIC_STD.ALL;
+
+entity reg_tb is
+end reg_tb;
+
+architecture Behavioral of reg_tb is
+
+constant clkPeriod: time := 10 ns;
+
+signal clk:  STD_LOGIC;
+signal en:  STD_LOGIC;
+signal rst:  STD_LOGIC;
+signal data_in:  STD_LOGIC_VECTOR(7 downto 0);
+signal data_out:  STD_LOGIC_VECTOR(7 downto 0);
+
+begin
+    dut: entity work.reg
+        port map(
+            clk => clk,
+            en => en,
+            rst => rst,
+            data_in => data_in,
+            data_out => data_out
+        );
+
+    clk_proc: process
+    begin
+        clk <= '0';
+        wait for clkPeriod / 2;
+        clk <= '1';
+        wait for clkPeriod / 2;
+    end process;
+
+    stimuli: process
+    begin
+        report "TESTING RAM WRITE AND READ";
+        rst <= '0';
+        data_in <= x"32";
+        en <= '1';
+        wait for 10 ns;
+        assert data_out = x"32" report "Failed to write" severity error;
+        wait for 90 ns;
+
+        report "TESTING WRITE ENABLE";
+        data_in <= x"33";
+        en <= '0';
+        wait for 10 ns;
+        assert data_out /= x"33" report "Write enable not working" severity error;
+        wait for 90 ns;
+
+        report "TESTING RESET SIGNAL";
+        rst <= '1';
+        en <= '1';
+        wait for 10 ns;
+        assert data_out = x"00" report "Reset signal not working" severity error;
+        wait for 90 ns;
+        
+        wait;
+    end process;
+end Behavioral;
