3f46e9b80ef277ad10baf718092ba831651b8cf1
Michael <mihalishatzoglakis@gmail.com>
2026-07-12 19:07:47 +0300

    Implement RAM initialization from 'program.hex'
    Added functionality to initialize RAM from a hex file.

--- design/ram.vhd
+++ design/ram.vhd
@@ -1,6 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use std.textio.all;
+use IEEE.std_logic_textio.all;
 
 entity ram is
     generic (
@@ -19,7 +21,28 @@ end ram;
 architecture Behavioral of ram is
 
     type ram_type is array (0 to (2**ADDRESS_WIDTH) - 1) of STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
-    signal ram: ram_type;
+    
+    --THIS FUNCTION IS USED TO LOAD THE PROGRAM "program.hex" TO RAM. TO CHANGE THE PROGRAM LOADED, CHANGE THE NAME IN THE FIRST LINE
+    --It doesnt run on the FPGA, but on the PC that's doing the synthesis
+    impure function init_ram_from_file return ram_type is
+        file text_file : text open read_mode is "program.hex";
+        variable text_line : line;
+        variable ram_content : ram_type := (others => (others => '0'));
+        variable hex_val : std_logic_vector(7 downto 0);
+    begin
+        for i in 0 to (2**ADDRESS_WIDTH) - 1 loop
+            if not endfile(text_file) then
+                readline(text_file, text_line);
+                hread(text_line, hex_val); -- Read the hex string into logic vector
+                ram_content(i) := hex_val;
+            else
+                exit;
+            end if;
+        end loop;
+        return ram_content;
+    end function;
+
+    signal ram: ram_type := init_ram_from_file;
 
 begin
 
