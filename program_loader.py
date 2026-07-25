44b79dc99688313e781d6177d78897774c2a9c70
Michael <mihalishatzoglakis@gmail.com>
2026-07-26 02:01:54 +0300

    Modify program_loader to read firmware from txt file
    Added functionality to convert binary strings to raw bytes for uploading.

--- program_loader.py
+++ program_loader.py
@@ -2,15 +2,26 @@ import serial
 import time
 import sys
 
-if len(sys.argv) != 1:
+if len(sys.argv) != 2:
     print("SYNTAX ERROR: NO PROGRAM GIVEN")
     sys.exit(1)
 
 ser = serial.Serial('COM3', 9600)
 time.sleep(2) # Give the port a second to initialize
 
-with open(sys.argv[0], "rb") as file:
-    firmware = file.read()
+with open(sys.argv[1], "r") as file:
+    lines = file.readlines()
+
+firmware = []
+#Convert the text into raw bytes
+for line in lines:
+    clean_line = line.strip()
+
+    # Skip any empty lines at the bottom of the text file
+    if len(clean_line) > 0:
+        # Convert the string (base 2) into a real integer
+        raw_byte = int(clean_line, 2)
+        firmware.append(raw_byte)
 
 print(f"Uploading {len(firmware)} bytes...")
 
