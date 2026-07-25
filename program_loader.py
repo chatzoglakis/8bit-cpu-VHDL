99c8b503e0820b046a0076d817d733d65cbdf019
Michael <mihalishatzoglakis@gmail.com>
2026-07-25 19:40:28 +0300

    Add files via upload

---
+++ program_loader.py
@@ -0,0 +1,23 @@
+import serial
+import time
+import sys
+
+if len(sys.argv) != 1:
+    print("SYNTAX ERROR: NO PROGRAM GIVEN")
+    sys.exit(1)
+
+ser = serial.Serial('COM3', 9600)
+time.sleep(2) # Give the port a second to initialize
+
+with open("program.bin", "rb") as file:
+    firmware = file.read()
+
+print(f"Uploading {len(firmware)} bytes...")
+
+# Send the bytes one by one
+for byte in firmware:
+    ser.write(bytes([byte]))
+    time.sleep(0.001) # Tiny pause to not overwhelm the FPGA
+
+print("Upload complete")
+ser.close()
